6142800256813c30f8326027df4a66f98cc74fe5
Thomas Schardt <30317420+thosch1800@users.noreply.github.com>
2017-09-14 10:35:32 +0200

    Update Uart.h

--- FT8800Mod/FT8800/Uart.h
+++ FT8800Mod/FT8800/Uart.h
@@ -3,8 +3,8 @@
 
 class Uart
 {
-    public: Write(uint8_t data);
-    public: Write(uint8_t data*, uint8_t dataLength);
+    public: void Write(uint8_t data);
+    public: void Write(uint8_t data*, uint8_t dataLength);
 }
 
 #endif
