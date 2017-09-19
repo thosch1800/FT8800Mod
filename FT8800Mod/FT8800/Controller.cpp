2058a6f28598319b2a89c884aabf23b77bcec32b
Thomas Schardt <30317420+thosch1800@users.noreply.github.com>
2017-09-19 15:01:24 +0200

    Update Controller.cpp
    added enum class

--- FT8800Mod/FT8800/Controller.cpp
+++ FT8800Mod/FT8800/Controller.cpp
@@ -26,17 +26,17 @@ void Controller::Turn(Direction direction, bool left)
 {
 	switch(direction)
 	{
-		case Left:
+		case Direction::Left:
 		if(left)	pPanel->DialLeft = 0xFF;
 		else		pPanel->DialRight = 0x7F;
 		break;
 		
-		case Right:
+		case Direction::Right:
 		if(left)	pPanel->DialLeft = 0x81;
 		else		pPanel->DialRight = 0x01;
 		break;
 
-		case None:
+		case Direction::None:
 		if(left)	pPanel->DialLeft = 0x80;
 		else		pPanel->DialRight = 0x00;
 		break;
@@ -47,23 +47,23 @@ void Controller::Press(Key key, bool left)
 {
 	switch(key)
 	{
-		case PTT: pPanel->PushToTalk = 0x1B; break;
+		case Key::PTT: pPanel->PushToTalk = 0x1B; break;
 
-		case Low: left ? pPanel->KeysLeft = 0x61 : pPanel->KeysRight = 0x00; break;
-		case VM:  left ? pPanel->KeysLeft = 0x40 : pPanel->KeysRight = 0x1F; break;
-		case HM:  left ? pPanel->KeysLeft = 0x1F : pPanel->KeysRight = 0x40; break;
-		case Scn: left ? pPanel->KeysLeft = 0x00 : pPanel->KeysRight = 0x61; break;
+		case Key::Low: left ? pPanel->KeysLeft = 0x61 : pPanel->KeysRight = 0x00; break;
+		case Key::VM:  left ? pPanel->KeysLeft = 0x40 : pPanel->KeysRight = 0x1F; break;
+		case Key::HM:  left ? pPanel->KeysLeft = 0x1F : pPanel->KeysRight = 0x40; break;
+		case Key::Scn: left ? pPanel->KeysLeft = 0x00 : pPanel->KeysRight = 0x61; break;
 
-		case Set:     pPanel->Keys = 0x04; break;
-		case DialKey: pPanel->Keys = left ? 0x02 : 0x01; break;
-		case VolSql:  if(left) pPanel->Keys = 0x08; break; //NOTE: right volume is power on/off
+		case Key::Set:     pPanel->Keys = 0x04; break;
+		case Key::DialKey: pPanel->Keys = left ? 0x02 : 0x01; break;
+		case Key::VolSql:  if(left) pPanel->Keys = 0x08; break; //NOTE: right volume is power on/off
 
-		case H1:  pPanel->Hyper = 0x01; break;
-		case H2:  pPanel->Hyper = 0x02; break;
-		case H3:  pPanel->Hyper = 0x03; break;
-		case H4:  pPanel->Hyper = 0x04; break;
-		case H5:  pPanel->Hyper = 0x05; break;
-		case H6:  pPanel->Hyper = 0x06; break;
+		case Key::H1:  pPanel->Hyper = 0x01; break;
+		case Key::H2:  pPanel->Hyper = 0x02; break;
+		case Key::H3:  pPanel->Hyper = 0x03; break;
+		case Key::H4:  pPanel->Hyper = 0x04; break;
+		case Key::H5:  pPanel->Hyper = 0x05; break;
+		case Key::H6:  pPanel->Hyper = 0x06; break;
 	}
 }
 
@@ -71,33 +71,33 @@ void Controller::Press(Number number)
 {
 	switch(number)
 	{
-		case Num1:    pPanel->Mic06 = 0x03; pPanel->Mic09 = 0x1B; break;
-		case Num2:    pPanel->Mic06 = 0x03; pPanel->Mic09 = 0x32; break;
-		case Num3:    pPanel->Mic06 = 0x03; pPanel->Mic09 = 0x4b; break;
-		case NumA:    pPanel->Mic06 = 0x03; pPanel->Mic09 = 0x66; break;
+		case Number::Num1:    pPanel->Mic06 = 0x03; pPanel->Mic09 = 0x1B; break;
+		case Number::Num2:    pPanel->Mic06 = 0x03; pPanel->Mic09 = 0x32; break;
+		case Number::Num3:    pPanel->Mic06 = 0x03; pPanel->Mic09 = 0x4b; break;
+		case Number::NumA:    pPanel->Mic06 = 0x03; pPanel->Mic09 = 0x66; break;
 		
-		case Num4:    pPanel->Mic06 = 0x19; pPanel->Mic09 = 0x1B; break;
-		case Num5:    pPanel->Mic06 = 0x19; pPanel->Mic09 = 0x32; break;
-		case Num6:    pPanel->Mic06 = 0x19; pPanel->Mic09 = 0x4B; break;
-		case NumB:    pPanel->Mic06 = 0x19; pPanel->Mic09 = 0x66; break;
+		case Number::Num4:    pPanel->Mic06 = 0x19; pPanel->Mic09 = 0x1B; break;
+		case Number::Num5:    pPanel->Mic06 = 0x19; pPanel->Mic09 = 0x32; break;
+		case Number::Num6:    pPanel->Mic06 = 0x19; pPanel->Mic09 = 0x4B; break;
+		case Number::NumB:    pPanel->Mic06 = 0x19; pPanel->Mic09 = 0x66; break;
 		
-		case Num7:    pPanel->Mic06 = 0x31; pPanel->Mic09 = 0x1B; break;
-		case Num8:    pPanel->Mic06 = 0x31; pPanel->Mic09 = 0x32; break;
-		case Num9:    pPanel->Mic06 = 0x31; pPanel->Mic09 = 0x4B; break;
-		case NumC:    pPanel->Mic06 = 0x31; pPanel->Mic09 = 0x66; break;
+		case Number::Num7:    pPanel->Mic06 = 0x31; pPanel->Mic09 = 0x1B; break;
+		case Number::Num8:    pPanel->Mic06 = 0x31; pPanel->Mic09 = 0x32; break;
+		case Number::Num9:    pPanel->Mic06 = 0x31; pPanel->Mic09 = 0x4B; break;
+		case Number::NumC:    pPanel->Mic06 = 0x31; pPanel->Mic09 = 0x66; break;
 		
-		case NumStar: pPanel->Mic06 = 0x4B; pPanel->Mic09 = 0x1C; break;
-		case Num0:    pPanel->Mic06 = 0x4B; pPanel->Mic09 = 0x33; break;
-		case NumHash: pPanel->Mic06 = 0x4B; pPanel->Mic09 = 0x4B; break;
-		case NumD:    pPanel->Mic06 = 0x4B; pPanel->Mic09 = 0x66; break;
+		case Number::NumStar: pPanel->Mic06 = 0x4B; pPanel->Mic09 = 0x1C; break;
+		case Number::Num0:    pPanel->Mic06 = 0x4B; pPanel->Mic09 = 0x33; break;
+		case Number::NumHash: pPanel->Mic06 = 0x4B; pPanel->Mic09 = 0x4B; break;
+		case Number::NumD:    pPanel->Mic06 = 0x4B; pPanel->Mic09 = 0x66; break;
 		
-		case NumP1:   pPanel->Mic06 = 0x63; pPanel->Mic09 = 0x1C; break;
-		case NumP2:   pPanel->Mic06 = 0x63; pPanel->Mic09 = 0x33; break;
-		case NumP3:   pPanel->Mic06 = 0x63; pPanel->Mic09 = 0x4B; break;
-		case NumP4:   pPanel->Mic06 = 0x63; pPanel->Mic09 = 0x66; break;
+		case Number::NumP1:   pPanel->Mic06 = 0x63; pPanel->Mic09 = 0x1C; break;
+		case Number::NumP2:   pPanel->Mic06 = 0x63; pPanel->Mic09 = 0x33; break;
+		case Number::NumP3:   pPanel->Mic06 = 0x63; pPanel->Mic09 = 0x4B; break;
+		case Number::NumP4:   pPanel->Mic06 = 0x63; pPanel->Mic09 = 0x66; break;
 		
-		case NumDown: pPanel->Mic06 = 0x1D; pPanel->Mic09 = 0x06; break;
-		case NumUp:   pPanel->Mic06 = 0x34; pPanel->Mic09 = 0x06; break;
+		case Number::NumDown: pPanel->Mic06 = 0x1D; pPanel->Mic09 = 0x06; break;
+		case Number::NumUp:   pPanel->Mic06 = 0x34; pPanel->Mic09 = 0x06; break;
 	}
 }
 
