4cabb3e7da0e1de9195053433980194c7a002905
Thomas Schardt <thomas.schardt@gmx.de>
2017-09-23 13:00:20 +0200

    simplified logic

--- FT8800Mod/FT8800/callbacks.c
+++ FT8800Mod/FT8800/callbacks.c
@@ -7,6 +7,8 @@
 extern PanelToMainUnitPacketBytes panel;
 extern MainUnitToPanelPacketBytes display;
 
+//NOTE: all callbacks run in context of ISRs, see timer.c for more detailed information
+
 inline void OnByteReceived0()
 {
     buffer0.Data[buffer0.Index++] = uart_getc();
@@ -19,27 +21,18 @@ inline void OnByteReceived1()
 
 inline void OnFrameReceived0()
 {
-    if(buffer0.Index >= sizeof(panel))
-    {
-        memcpy(&panel, buffer0.Data, sizeof(panel));
+    if(buffer0.Index < sizeof(panel)) return; // continue collect data
 
-        Reset(&buffer0);
-    }
+    if(buffer0.Index == sizeof(panel)) memcpy(&panel, buffer0.Data, sizeof(panel)); // copy frame data
+
+    buffer0.Index = 0; // reset index (no matter if packet received or not!)
 }
 
 inline void OnFrameReceived1()
 {
-    if(buffer1.Index >= sizeof(display))
-    {
-        memcpy(&display, buffer1.Data, sizeof(display));
+    if(buffer1.Index < sizeof(display)) return; // continue collect data
 
-        Reset(&buffer1);
-    }
-}
+    if(buffer1.Index == sizeof(display)) memcpy(&display, buffer1.Data, sizeof(display)); // copy frame data
 
-inline void Reset(Buffer* buffer)
-{
-    memset(buffer->Data, 0, sizeof(buffer->Data));
-
-    buffer->Index = 0;
+    buffer1.Index = 0; // reset index (no matter if packet received or not!)
 }
