b14de05fcb99c2a3ceeba1358ac9dbf6b220f590
Thomas Schardt <thomas.schardt@gmx.de>
2017-09-25 10:40:01 +0200

    merged to utilities

--- FT8800Mod/FT8800/uartUtils.c
+++
@@ -1,8 +0,0 @@
-#include "uartUtils.h"
-#include "uart.h"
-
-void InitializeUart()
-{
-    uart_init(UART_BAUD_SELECT(19200UL, F_CPU));
-    uart1_init(UART_BAUD_SELECT(19200UL, F_CPU));
-}
--- FT8800Mod/FT8800/uartUtils.h
+++
@@ -1,6 +0,0 @@
-#ifndef UARTUTILS_H_
-#define UARTUTILS_H_
-
-void InitializeUart();
-
-#endif /* UARTUTILS_H_ */
