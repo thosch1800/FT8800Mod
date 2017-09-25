547c19b6c5ee76726f650f719ad4a949ce2e5311
Thomas Schardt <thomas.schardt@gmx.de>
2017-09-25 10:03:53 +0200

    added documentation

--- FT8800Mod/FT8800/Main.cpp
+++ FT8800Mod/FT8800/Main.cpp
@@ -15,18 +15,6 @@ extern "C"
 PanelToMainUnitPacketBytes panel;
 MainUnitToPanelPacketBytes display;
 
-/*  changes made to uart.h/uart.c
-
-    uart.h: 
-    increased buffer size
-    #define UART_TX_BUFFER_SIZE 128
-
-    uart.c:
-    added callbacks to ISRs
-    ISR (UART0_RECEIVE_INTERRUPT) => RestartTimer0();
-    ISR (UART1_RECEIVE_INTERRUPT) => RestartTimer1();
-*/
-
 int main()
 {
     #ifdef MEASURE_TIMINGS
@@ -56,9 +44,44 @@ int main()
         display.SignalLeftBar5 << PINA4 |
         display.SignalLeftBar6 << PINA5 |
         display.SignalLeftBar7 << PINA6 |
-        display.SignalLeftBar8 << PINA7 
+        display.SignalLeftBar8 << PINA7
         );
     }
 
+    while(true) { }
     return 0;
 }
+
+/*
+Requirements:
+- do not miss a panel packet because it would "eat up" user input
+- display packets can be dropped, because they will be repeated
+
+Interrupt priority:
+12 0x016 TIMER2 OVF Timer/Counter2 Overflow
+18 0x022 TIMER0 OVF Timer/Counter0 Overflow
+20 0x026 USART0, RXC USART0, Rx Complete
+21 0x028 USART1, RXC USART1, Rx Complete
+
+Conclusion:
+Timer2 for USART0 to handle the panel packets
+Timer0 for USART1 to handle display packets
+
+Whenever a byte was received by UART we start the corresponding timer.
+Thus if the following byte is received until the timer overflowed the timer is reset.
+If there is no byte received within the timer period we assume a complete frame.
+
+
+
+
+changes made to uart.h/uart.c
+
+uart.h:
+increased buffer size
+#define UART_TX_BUFFER_SIZE 128
+
+uart.c:
+added callbacks to ISRs
+ISR (UART0_RECEIVE_INTERRUPT) => OnByteReceivedUart0();
+ISR (UART1_RECEIVE_INTERRUPT) => OnByteReceivedUart1();
+*/
